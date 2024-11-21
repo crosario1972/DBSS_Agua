f7b530260d8f147d2ac7648e41e520337c83699d
Carlos Rosario <crosario.dbss@gmail.com>
2024-11-21 15:50:32 -0400

    2024-11-21-3:50pm

--- DBSS_Agua.API2/Controllers/ClientesController.cs
+++ DBSS_Agua.API2/Controllers/ClientesController.cs
@@ -1,15 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using DBSS_Agua.API.Models;
 using System.Data;
 using System.Data.Entity;
 using System.Data.Entity.Infrastructure;
 using System.Linq;
 using System.Net;
-using System.Net.Http;
 using System.Threading.Tasks;
 using System.Web.Http;
 using System.Web.Http.Description;
-using DBSS_Agua.API.Models;
 
 namespace DBSS_Agua.API2.Controllers
 {
